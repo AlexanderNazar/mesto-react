3e4f565f2ea776ddaa91b86000e3ddbfcdcf9078
Alexander Nazarov <naa@oplot48.ru>
2022-06-25 00:46:22 +0300

    feat: сдача на 1 проверку 10 ПР

--- src/components/PopupImageOpen.jsx
+++ src/components/PopupImageOpen.jsx
@@ -1,6 +1,6 @@
 function PopupImageOpen(props) {
   return (
-    <div key={props.card._id} className={`popup popup_type_preview ${!!props.card ? "popup_opened" : ""}`}>
+    <div className={`popup popup_type_preview ${!!props.card ? "popup_opened" : ""}`}>
       <div className="popup__container-image">
         <button className="popup__close-button" type="button" onClick={props.onClose}></button>
         <img
